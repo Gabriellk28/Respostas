dd36a443a7fcab1f4295d695e6e360098fe2e4e3
Uyanê Gabriel Lima da Silva <115796345+Gabriellk28@users.noreply.github.com>
2023-07-25 17:34:48 -0300

    Add files via upload

--- enumQ3.c
+++ enumQ3.c
@@ -2,25 +2,24 @@
 
 typedef enum mes
 {
-   janeiro = 01,
-   fevereiro = 02,
-   março= 2,
-   abril= 02,
-   maio= 02,
-   junho= 02,
-   julho= 02,
-   agosto= 02,
-   setembro= 02,
-   outrubro= 02,
-   dezembro= 02
+   janeiro,
+   fevereiro,
+   março,
+   abril,
+   maio,
+   junho,
+   julho,
+   agosto,
+   setembro,
+   outrubro,
+   dezembro,
 }Mes;
-
 
 typedef struct data
 {
-    char dia[5];
+    int dia;
     Mes mes;
-    char ano[5];
+    int ano;
 }Data;
 
 int main(void)
@@ -28,13 +27,14 @@ int main(void)
     Data data;
 
     printf("Informe o dia:\n");
-    scanf(" %[^\n]", &data.dia);
+    scanf(" %d", &data.dia);
     printf("Informe o mes:\n");
     scanf(" %d", &data.mes);
     printf("Informe o ano:\n");
-    scanf(" %f", &data.ano);
+    scanf(" %d", &data.ano);
 
     printf("Data:\n");
+    printf("%d/%d/%d\n", data.dia, data.mes, data.ano);
 
     return 0;
 }
